26930536a2074de345eaff25d61b3bf166e0ba1d
etypeweb@jackie <etypeweb@jackie>
2020-05-12 08:21:36 -0500

    updates

--- web/sites/sites.php
+++ web/sites/sites.php
@@ -3,34 +3,6 @@ $sites['cninewspapers.etypegoogle11.com'] = "cninewspapers.etypegoogle11.com";
 $sites['www.cninewspapers.com'] = "cninewspapers.etypegoogle11.com";
 $sites['cninewspapers.com'] = "cninewspapers.etypegoogle11.com";
 
-$sites['myfamilydocga.etypegoogle11.com'] = "myfamilydocga.etypegoogle11.com";
-$sites['www.myfamilydocga.com'] = "myfamilydocga.etypegoogle11.com";
-$sites['myfamilydocga.com'] = "myfamilydocga.etypegoogle11.com";
-
-$sites['taniafrench.etypegoogle11.com'] = "taniafrench.etypegoogle11.com";
-$sites['www.taniafrench.com'] = "taniafrench.etypegoogle11.com";
-$sites['taniafrench.com'] = "taniafrench.etypegoogle11.com";
-
-$sites['netxcrossroads.etypegoogle11.com'] = "netxcrossroads.etypegoogle11.com";
-$sites['www.netxcrossroads.com'] = "netxcrossroads.etypegoogle11.com";
-$sites['netxcrossroads.com'] = "netxcrossroads.etypegoogle11.com";
-
-$sites['bellvilletimes.etypegoogle11.com'] = "bellvilletimes.etypegoogle11.com";
-$sites['www.bellvilletimes.com'] = "bellvilletimes.etypegoogle11.com";
-$sites['bellvilletimes.com'] = "bellvilletimes.etypegoogle11.com";
-
-$sites['66hoursingrants.etypegoogle11.com'] = "66hoursingrants.etypegoogle11.com";
-$sites['www.66hoursingrants.com'] = "66hoursingrants.etypegoogle11.com";
-$sites['66hoursingrants.com'] = "66hoursingrants.etypegoogle11.com";
-
-$sites['goodnewspress.etypegoogle11.com'] = "goodnewspress.etypegoogle11.com";
-$sites['www.goodnewspress.com'] = "goodnewspress.etypegoogle11.com";
-$sites['goodnewspress.com'] = "goodnewspress.etypegoogle11.com";
-
-$sites['hoxietimes.etypegoogle11.com'] = "hoxietimes.etypegoogle11.com";
-$sites['www.hoxietimes.com'] = "hoxietimes.etypegoogle11.com";
-$sites['hoxietimes.com'] = "hoxietimes.etypegoogle11.com";
-
 $sites['kindernow.etypegoogle11.com'] = "kindernow.etypegoogle11.com";
 $sites['www.kindernow.com'] = "kindernow.etypegoogle11.com";
 $sites['kindernow.com'] = "kindernow.etypegoogle11.com";
@@ -43,45 +15,9 @@ $sites['westcarrollgazette.etypegoogle11.com'] = "westcarrollgazette.etypegoogle
 $sites['www.westcarrollgazette.com'] = "westcarrollgazette.etypegoogle11.com";
 $sites['westcarrollgazette.com'] = "westcarrollgazette.etypegoogle11.com";
 
-$sites['sabinecountyreporter.etypegoogle11.com'] = "sabinecountyreporter.etypegoogle11.com";
-$sites['www.sabinecountyreporter.com'] = "sabinecountyreporter.etypegoogle11.com";
-$sites['sabinecountyreporter.com'] = "sabinecountyreporter.etypegoogle11.com";
-
 $sites['themillerpress.etypegoogle11.com'] = "themillerpress.etypegoogle11.com";
 $sites['www.themillerpress.com'] = "themillerpress.etypegoogle11.com";
 $sites['themillerpress.com'] = "themillerpress.etypegoogle11.com";
-
-$sites['theravennanews.etypegoogle11.com'] = "theravennanews.etypegoogle11.com";
-$sites['www.theravennanews.com'] = "theravennanews.etypegoogle11.com";
-$sites['theravennanews.com'] = "theravennanews.etypegoogle11.com";
-
-$sites['thekansascityglobe.etypegoogle11.com'] = "thekansascityglobe.etypegoogle11.com";
-$sites['www.thekansascityglobe.com'] = "thekansascityglobe.etypegoogle11.com";
-$sites['thekansascityglobe.com'] = "thekansascityglobe.etypegoogle11.com";
-
-$sites['thekcglobe.etypegoogle11.com'] = "thekansascityglobe.etypegoogle11.com";
-$sites['www.thekcglobe.com'] = "thekansascityglobe.etypegoogle11.com";
-$sites['thekcglobe.com'] = "thekansascityglobe.etypegoogle11.com";
-
-$sites['phonographherald.etypegoogle11.com'] = "phonographherald.etypegoogle11.com";
-$sites['www.phonographherald.com'] = "phonographherald.etypegoogle11.com";
-$sites['phonographherald.com'] = "phonographherald.etypegoogle11.com";
-
-$sites['edenecho.etypegoogle11.com'] = "edenecho.etypegoogle11.com";
-$sites['www.edenecho.net'] = "edenecho.etypegoogle11.com";
-$sites['edenecho.net'] = "edenecho.etypegoogle11.com";
-
-$sites['normangeestar.etypegoogle11.com'] = "normangeestar.etypegoogle11.com";
-$sites['www.normangeestar.com'] = "normangeestar.etypegoogle11.com";
-$sites['normangeestar.com'] = "normangeestar.etypegoogle11.com";
-
-$sites['lulingnewsboy.etypegoogle11.com'] = "lulingnewsboy.etypegoogle11.com";
-$sites['www.lulingnewsboy.com'] = "lulingnewsboy.etypegoogle11.com";
-$sites['lulingnewsboy.com'] = "lulingnewsboy.etypegoogle11.com";
-
-$sites['lavacacountytoday.etypegoogle11.com'] = "lavacacountytoday.etypegoogle11.com";
-$sites['www.lavacacountytoday.com'] = "lavacacountytoday.etypegoogle11.com";
-$sites['lavacacountytoday.com'] = "lavacacountytoday.etypegoogle11.com";
 
 $sites['whitneytheatre.etypegoogle11.com'] = "whitneytheatre.etypegoogle11.com";
 $sites['www.whitneytheatre.com'] = "whitneytheatre.etypegoogle11.com";
@@ -91,35 +27,11 @@ $sites['texas-grass.etypegoogle11.com'] = "texas-grass.etypegoogle11.com";
 $sites['www.texas-grass.com'] = "texas-grass.etypegoogle11.com";
 $sites['texas-grass.com'] = "texas-grass.etypegoogle11.com";
 
-$sites['bookernews.etypegoogle11.com'] = "bookernews.etypegoogle11.com";
-$sites['www.bookernews.com'] = "bookernews.etypegoogle11.com";
-$sites['bookernews.com'] = "bookernews.etypegoogle11.com";
-
 $sites['wespubco.etypegoogle11.com'] = "wespubco.etypegoogle11.com";
 $sites['www.wespubco.com'] = "wespubco.etypegoogle11.com";
 $sites['wespubco.com'] = "wespubco.etypegoogle11.com";
 $sites['www.wesnerpublications.com'] = "wespubco.etypegoogle11.com";
 $sites['wesnerpublications.com'] = "wespubco.etypegoogle11.com";
-
-$sites['mosercommedia.etypegoogle11.com'] = "mosercommedia.etypegoogle11.com";
-$sites['www.mosercommedia.com'] = "mosercommedia.etypegoogle11.com";
-$sites['mosercommedia.com'] = "mosercommedia.etypegoogle11.com";
-
-$sites['giddingstimes.etypegoogle11.com'] = "giddingstimes.etypegoogle11.com";
-$sites['www.giddingstimes.com'] = "giddingstimes.etypegoogle11.com";
-$sites['giddingstimes.com'] = "giddingstimes.etypegoogle11.com";
-
-$sites['colfaxchronicle.etypegoogle11.com'] = "colfaxchronicle.etypegoogle11.com";
-$sites['www.colfaxchronicle.com'] = "colfaxchronicle.etypegoogle11.com";
-$sites['colfaxchronicle.com'] = "colfaxchronicle.etypegoogle11.com";
-
-$sites['columbusnews-report.etypegoogle11.com'] = "columbusnews-report.etypegoogle11.com";
-$sites['www.columbusnews-report.com'] = "columbusnews-report.etypegoogle11.com";
-$sites['columbusnews-report.com'] = "columbusnews-report.etypegoogle11.com";
-
-$sites['rileycountian.etypegoogle11.com'] = "rileycountian.etypegoogle11.com";
-$sites['www.rileycountian.com'] = "rileycountian.etypegoogle11.com";
-$sites['rileycountian.com'] = "rileycountian.etypegoogle11.com";
 
 $sites['unwindnorthgeorgia.etypegoogle11.com'] = "ung.etypegoogle11.com";
 $sites['www.unwindnorthgeorgia.com'] = "ung.etypegoogle11.com";
@@ -133,28 +45,13 @@ $sites['taylornews.etypegoogle11.com'] = "taylornews.etypegoogle11.com";
 $sites['www.taylornews.org'] = "taylornews.etypegoogle11.com";
 $sites['taylornews.org'] = "taylornews.etypegoogle11.com";
 
-$sites['runwithsam.etypegoogle11.com'] = "runwithsam.etypegoogle11.com";
-$sites['www.runwithsam.org'] = "runwithsam.etypegoogle11.com";
-$sites['runwithsam.org'] = "runwithsam.etypegoogle11.com";
-
 $sites['chevroletalto.etypegoogle11.com'] = "chevroletalto.etypegoogle11.com";
 $sites['www.chevroletalto.com'] = "chevroletalto.etypegoogle11.com";
 $sites['chevroletalto.com'] = "chevroletalto.etypegoogle11.com";
 
-$sites['belleplainenews.etypegoogle11.com'] = "belleplainenews.etypegoogle11.com";
-$sites['www.belleplainenews.com'] = "belleplainenews.etypegoogle11.com";
-$sites['belleplainenews.com'] = "belleplainenews.etypegoogle11.com";
-
 $sites['lakelife.etypegoogle11.com'] = "lakelife.etypegoogle11.com";
 $sites['www.lakelife.today'] = "lakelife.etypegoogle11.com";
 $sites['lakelife.today'] = "lakelife.etypegoogle11.com";
-
-$sites['www.thewewokatimes.com'] = "thewewokatimes.etypegoogle11.com";
-$sites['thewewokatimes.com'] = "thewewokatimes.etypegoogle11.com";
-
-$sites['konawaleader.etypegoogle11.com'] = "konawaleader.etypegoogle11.com";
-$sites['www.konawaleader.com'] = "konawaleader.etypegoogle11.com";
-$sites['konawaleader.com'] = "konawaleader.etypegoogle11.com";
 
 $sites['theanahuacprogress.etypegoogle11.com'] = "theanahuacprogress.etypegoogle11.com";
 $sites['www.theanahuacprogress.com'] = "theanahuacprogress.etypegoogle11.com";
